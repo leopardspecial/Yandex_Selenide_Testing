cf4bb539d114bab2d168b5d60423f987d5b59755
leopardegorl <leopadegorl@gmail.com>
2020-10-13 01:52:14 +0400

    final commit

--- src/main/java/page/Main.java
+++
@@ -1,9 +0,0 @@
-package page;
-
-public class Main {
-    public static void main(String[] args) {
-        for (int k = 4; k < 4 + 4; k++) {
-            System.out.println(k);
-        }
-    }
-}
